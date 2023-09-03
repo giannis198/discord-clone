d01ff3de0bedb37d5921a269973ba1e2e642897b
giannis198 <john_nik@windowslive.com>
2023-09-03 22:19:19 +0300

    Search field finished

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -9,11 +9,12 @@ export async function PATCH(
   try {
     const profile = await currentProfile();
 
-    console.log("Server Id", params.serverId);
-
+   
     const { name, imageUrl } = await req.json();
 
     if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    
 
     const server = await db.server.update({
       where: {
@@ -29,6 +30,35 @@ export async function PATCH(
     return NextResponse.json(server);
   } catch (error) {
     console.log("[SERVER_ID_PATCH", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    if (!params.serverId)
+      return new NextResponse("Server ID Missing", { status: 400 });
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
