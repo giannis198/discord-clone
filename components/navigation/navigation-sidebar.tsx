6a29d6fafe23d80afc577b8bc1ed84b4b94943b2
giannis198 <john_nik@windowslive.com>
2023-09-12 23:33:03 +0300

    deployed commit2

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -48,7 +48,7 @@ const NavigationSidebar = async () => {
             },
           }}
         />
-        <span className="text-center">Created by Ioannis ©2023</span>
+        <span className="text-center">Created by Ioannis @2023</span>
       </div>
     </div>
   );
